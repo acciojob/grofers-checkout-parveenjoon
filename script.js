336d8a4729c78af065ba3236718c5c62519136a2
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-05-31 10:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
 
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+//your code here
+const priceArray=document.querySelectorAll('[data-ns-test="price"]');
+let table=document.getElementById("table1");
+let ans=0;
+for (let i = 0; i < priceArray.length; i++) {
+	ans+=parseInt(priceArray[i].innerText);
+}
+let total=document.createElement("tr");
 
-const getSum = () => {
-//Add your code here
-	let prices=document.getElementsByClassName("price");
-	let n=prices.length;
-	let ans=0;
-	for(let i=0;i<n;i++)
-		{
-			let price=(Number)(prices.item(i).innerText);
-			ans+=price;
-        }
-	
-	let table=document.getElementsByTagName("table").item(0);
-	
-	 let row=document.createElement("tr");
-	let d1=document.createElement("td");
-	let d2=document.createElement("td");
-	d1.innerText="total price";
-	row.appendChild(d1);
-	d2.id="ans";
-	d2.innerText=ans;
-	row.appendChild(d2);
-	table.appendChild(row);
-	
-  
-};
+total.setAttribute("data-ns-test", "grandTotal");
 
-getSumBtn.addEventListener("click", getSum,{once:true});
+total.innerText=ans;
+
+table.appendChild(total);
